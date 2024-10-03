6c0021f73a9a8c25dba43e099655bf15e1c42b7e
Jefferson Carvalho <contato.jcarvalho@outlook.com>
2024-10-03 20:10:46 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// Para o link da Política de Privacidade
+    const politicaPrivacidadeLink = document.getElementById("politica-privacidade");
+    politicaPrivacidadeLink.addEventListener("click", function(event) {
+        event.preventDefault(); // Impede que o link abra imediatamente
+        alert("A Política de privacidade não existe ainda. O link é apenas para demonstração do escopo integral do site para a Atividade Extensionista. .");
+    });
